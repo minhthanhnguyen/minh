c0d50caa7ebda26a362667531820331d93bec6ee
Minh Nguyen <NGuyen@win.cccis.com>
2018-11-14 22:09:00 -0800

    Adding Sendgrid test

---
+++ minhCore1/Services/SendGridService.cs
@@ -0,0 +1,32 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace minhCore1.Services
+{
+    public static class SendGridService
+    {
+        public static async Task SendTestEmail()
+        {
+            try
+            {
+                var client = new SendGridClient("");
+
+                // Send a Single Email using the Mail Helper
+                var from = new EmailAddress("minhthanhnguyen82@gmail.com", "Minh Nguyen");
+                var subject = "Testing email!";
+                var to = new EmailAddress("minhthanhnguyen82@gmail.com", "Minh Nguyen");
+                var plainTextContent = "Testing email content!";
+                var htmlContent = "<strong>Testing email content!</strong>";
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+
+                var response = await client.SendEmailAsync(msg);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
